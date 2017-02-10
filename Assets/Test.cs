d827f8f72a7da3596721a869e0c2f2f15821d076
khtaaa <kahata1128@gmail.com>
2017-02-10 11:40:19 +0900

    追加2

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class Test : MonoBehaviour 
 {
     public GameObject cube;
-
+    public GameObject tama;
+    public GameObject capuseru;
+    int rand;
+    int time = 0;
 	// Update is called once per frame
 	void Update () 
 	{
+        time++;
 		Quaternion now = transform.rotation;
 		Quaternion rot = Quaternion.AngleAxis(90f*Time.deltaTime, Vector3.up);
 		rot = rot*now;
 		transform.rotation = rot;
-        Instantiate(cube);
-        Instantiate(cube, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+
+        if (time == 180)
+        {
+            time = 0;
+            rand = Random.Range(1, 3);
+            if (rand == 1)
+            {
+                Instantiate(cube, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+                Instantiate(cube, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+
+            } else if (rand == 2)
+            {
+                Instantiate(tama, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+                Instantiate(tama, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+            } else if (rand == 3)
+            {
+                Instantiate(capuseru, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+                Instantiate(capuseru, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+            }
+        }
 	}
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
 }
